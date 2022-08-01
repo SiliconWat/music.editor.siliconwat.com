0b5208bbfaa205c6a1ef6e64ba05419519b17f74
Thon Ly <thonly@heartbank.ceo>
2022-08-01 15:32:26 -0700

    add remote components

--- components/sw-editor/template.mjs
+++ components/sw-editor/template.mjs
@@ -1,8 +1,9 @@
+const origin = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5508" : "https://music.siliconwat.com";
 const template = document.createElement("template");
 
 template.innerHTML = `
-    <link rel="stylesheet" href="components/sw-editor/sheets.css">
-    <link rel="stylesheet" href="components/sw-editor/notes.css">
+    <link rel="stylesheet" href="${origin}/components/sw-editor/sheets.css">
+    <link rel="stylesheet" href="${origin}/components/sw-editor/notes.css">
     <section>
         <ol></ol>
         <ol>
