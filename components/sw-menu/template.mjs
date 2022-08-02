e0fc47ea6d569bf9c515540db04028f9d505d597
Thon Ly <thonly@heartbank.ceo>
2022-08-01 18:35:02 -0700

    refactor components

--- components/sw-menu/template.mjs
+++ components/sw-menu/template.mjs
@@ -1,8 +1,7 @@
-const origin = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5508" : "https://music.siliconwat.com";
 const template = document.createElement("template");
 
 template.innerHTML = `
-    <link rel="stylesheet" href="${origin}/components/sw-menu/shadow.css">
+    <link rel="stylesheet" href="components/sw-menu/shadow.css">
     <section>
         <ul>
             <li>
