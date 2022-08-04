ec956e995f84dffa515857b49676d7cf04ce14cd
Thon Ly <thonly@heartbank.ceo>
2022-08-03 18:23:45 -0700

    begin events

--- components/sw-editor/element.mjs
+++ components/sw-editor/element.mjs
@@ -6,4 +6,10 @@ export class SwEditor extends HTMLElement {
         this.attachShadow({ mode: "open" });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
+
+    connectedCallback() {
+        this.shadowRoot.querySelector('section').onclick = () => {
+            this.dispatchEvent(new Event('correct', { bubbles: true, composed: true }));
+        }
+    }
 }
